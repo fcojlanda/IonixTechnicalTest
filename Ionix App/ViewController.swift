bfcb8fba76ba34f1757dd65b65619cd533b0fb33
Landatech <admin@landatech.com.mx>
2021-08-03 20:49:21 -0500

    Push inicial

---
+++ Ionix App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ionix App
+//
+//  Created by Francisco Landa Torres on 03/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
